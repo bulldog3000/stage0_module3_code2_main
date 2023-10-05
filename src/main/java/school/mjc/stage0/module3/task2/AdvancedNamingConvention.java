b527e1044e2eff1a12396477d1fc910f965cb61a
bulldog3000 <128056980+bulldog3000@users.noreply.github.com>
2023-10-05 12:51:58 +0300

    converting_names

---
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -0,0 +1,16 @@
+package school.mjc.stage0.module3.task2;
+
+public class AdvancedNamingConvention {
+    private static final int ADULT_AGE = 18;
+
+    private int age;
+    private int phoneNumber;
+
+    void callToFriend() {
+        //some code
+    }
+
+    void callByNumber(int number) {
+        //some code
+    }
+}
---
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -0,0 +1,14 @@
+package school.mjc.stage0.module3.task2;
+
+public class CopiedNames {
+    public static void main(String[] args) {
+        int variable_1 = 0;
+        int variable_2 = 2000;
+        int value = 1000;
+        int variable_3 = 4000;
+        int value_2 = 3000;
+        int variables = 5000;
+        variables = 7000;
+        int value_3 = 90000;
+    }
+}
